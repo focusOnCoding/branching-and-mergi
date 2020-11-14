49aa1cca79d0096fa4d23247eb0e5baebbc2438b
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-11-15 01:52:53 +0200

    after adding 15 lines of code just came back from th mosh remote repo

---
+++ remote.ps1
@@ -0,0 +1,15 @@
+# git remote | -v
+git remote -v
+
+# if there a any changes to the remote repo i must run
+git fetch origin # so every is upto date
+
+# then to make the origin and the master === use
+git merge origin/main # now everything is #!SYNC    
+# git pull joins the to commands {fetch and merge}
+git pull
+
+# then add some changes localy 
+
+# if i want to push them up to the remote repo
+git push
