e7107639f22d11adec861e2f60356144484f1f2b
Siyabonga Dlamini <focusoncoding@gmail.com>
2020-11-15 02:30:12 +0200

    done with remots

--- remote.ps1
+++ remote.ps1
@@ -13,3 +13,26 @@ git pull
 
 # if i want to push them up to the remote repo
 git push
+
+# add remote repo aliase named #!{upstream}
+git remote add upstream <url>
+# to remove a remote repo
+git remote remove <url | name>
+
+# fetch from the upstrem url
+git fetch upstream #!SYNC 
+
+# push to origin master
+git push origin master # now the master is in sync with remote repo
+
+# so in order to git the changes made to the original repo not the forked repo
+git fetch upstream
+# then merge commit from upstream to master | main branch
+git merge upstream/master # merged local ===
+
+# then to make the remote fork repo of the original === with my local and the main fork 
+#repo 
+git push origin master
+
+# remote branches
+  
